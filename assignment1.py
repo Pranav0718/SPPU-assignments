e1294674f985e3bdf2fe224e9f8a7c3a315c8206
Pranav  Walgude <pranavwalgude3177@gmail.com>
2021-11-18 23:34:46 +0530

    Update assignment1.py
    problem statment 
    *Write a python program to store marks scored in subject fds by N number of student in the class.Write Function to compute following
    a)The average score of class
    b)highest score and lowest score of the class
    c)display marks with highest frequency.

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,27 @@
+#function to calculate average
 
+def average(m):
+  sum=0
+  absent=0
+  for(int i in range(0,len(m))):
+    absent=absent+1
+    else:
+      sum=sum+m[1]
+      avg=sum/(len(m)-absent)
+      print("average marks are" avg )
+      
+      
+      #Function to calculate highest and lowest marks in the class
+      
+      def highestandlowest(m):
+        high=0
+        low=100
+        for i in range (0,len(m)):
+          if(type(m[i]!=type("")):
+             if(m[i]>high):
+             high=m[i]
+             if((m[i]<low)and type (m[i])!=type("")):
+             low=m[i]
+             print("/n highest score is:",high)
+             
+             
